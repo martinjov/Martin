526d22c073683551c1c32d3c96c5d82eb338752e
Martin <106605929+martinjov@users.noreply.github.com>
2022-12-04 20:06:18 +0100

    Add files via upload
    My first neural network. Mathematics, matrices and Numpy based. One hidden layer only.

---
+++ martin_neural_network_numpy.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+This is a simple digit recognition solution. One hidden layer only. Famous MNIST dataset is used.
+Done predominantly with mathematics, with help of numpy python library. Good precision despite one hidden layer.
+"""
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+data = pd.read_csv('C:/Users/TRC PRO/Desktop/python_projects/digit-recognizer/train.csv')
+
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
+def ReLU(Z):
+    return np.maximum(Z, 0)
+
+def softmax(Z):
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+
+def parameters_initialization():
+    W1 = np.random.normal(size=(10, 784)) * np.sqrt(1./(784))
+    b1 = np.random.normal(size=(10, 1)) * np.sqrt(1./10)
+    W2 = np.random.normal(size=(10, 10)) * np.sqrt(1./20)
+    b2 = np.random.normal(size=(10, 1)) * np.sqrt(1./(784))
+    return W1, b1, W2, b2
+
+    
+def forward_propagation(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def ReLU_deriv(Z):
+    return Z > 0
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1    
+    W2 = W2 - alpha * dW2  
+    b2 = b2 - alpha * db2    
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = parameters_initialization()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_propagation(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
+
+def prediction_making(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_propagation(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+def prediction_testing(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = prediction_making(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+    
+prediction_testing(0, W1, b1, W2, b2)
+
+dev_predictions = prediction_making(X_dev, W1, b1, W2, b2)
+
+get_accuracy(dev_predictions, Y_dev)
+
